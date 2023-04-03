8e57eea2729c6238de23018e13da970647b81a35
Matias Villar <95631527+matvillar@users.noreply.github.com>
2023-04-03 11:13:38 -0600

    Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 import { Switch, Route } from 'react-router-dom';
-import Plants from "./components/Plants";
-import Plant from "./components/Plant";
-import Cart from "./components/Cart";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Plants from './components/Plants';
+import Plant from './components/Plant';
+import About from './components/About';
+import Contact from './components/Contact';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import Register from './components/Register';
 function App() {
   return (
     <>
@@ -18,6 +20,8 @@ function App() {
         <Route exact path="/cart" component={Cart} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </>
   );
